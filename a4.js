e347c97190d86a57bb10965a342fa83adc7652af
dallen <dmarquisallen@gmail.com>
2015-02-09 12:48:25 -0600

    added part 3 of assignment 4

--- a4.js
+++ a4.js
@@ -10,3 +10,9 @@
  <script type="text/javascript">
      alert(document.title);
  </script>
+
+//A4.3
+ function validateEmail(email) { 
+     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+         return re.test(email);
+ }
