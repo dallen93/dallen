a94f646809ca2a2872fda920336be6694aac0334
dallen <dmarquisallen@gmail.com>
2015-02-11 00:49:13 -0600

    added assignment 4 part 4

--- a4.js
+++ a4.js
@@ -16,3 +16,15 @@
      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
          return re.test(email);
  }
+
+//A4.4
+
+ var xmlhttp2 = new XMLHttpRequest();
+ xmlhttp2.onreadystatechange = function(){
+ 		if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200){
+				console.log(xmlhttp2.responseText);
+						}
+						};
+
+						xmlhttp2.open("GET", "http://private-40e0f-smugui2015.apiary-mock.com/assignments/4", true);
+						xmlhttp2.send();
