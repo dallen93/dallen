aee24037d6e9a6a8487a6f4fc2dbb5dfd789b76d
D'Marquis Allen <dmarquisallen@gmail.com>
2015-04-17 22:48:01 -0500

    working on a6

--- Mustache/controller.js
+++ Mustache/controller.js
@@ -1,1 +1,27 @@
+function viewCart() {
+$.get('merchandise-list.mst', function(template) {
+    $.getJSON(
+        'http://private-54400-dallengui2015.apiary-mock.com/cart', 
+        {}, 
+        function(json, textStatus) {
+            var data = {"cart":json};
+            console.log(data);
+            var rendered = Mustache.render(template,data);
+            $('#body').html(rendered);
+        });
+    });
+}
+
+function addMerchandise() {
+$.get('add-merchandise.mst', function(template) {
+    $.getJSON(
+        'http://private-54400-dallengui2015.apiary-mock.com/cart',
+        {}, 
+        function(json, textStatus) {
+            var data = {"cart":json};
+            var rendered = Mustache.render(template,data);
+            $('#body').html(rendered);
+        });
+    });
+}
 
